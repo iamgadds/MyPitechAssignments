caf4bb5bff2ab7f28ba2420e22219e35c4d69451
iamgadds <husain.ghadiyali07@gmail.com>
2021-08-05 18:40:23 +0530

    assignment 2 commit

---
+++ ArrayAssignment/ArrayMinMax.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAssignment
+{
+    class ArrayMinMax
+    {
+        public int getValueUsingRef(int[] sort, ref int max)
+        {
+            int min = sort[0];
+            for(int i=1; i<sort.Length; i++){
+                if (sort[i] < min)
+                {
+                    min = sort[i];
+                }
+                else
+                {
+                    max = sort[i];
+                }
+            }
+            return min;
+        }
+
+        public int getValueUsingOut(int[] sort, out int max)
+        {
+            int min = sort[0];
+            max = sort[0];
+            for (int i = 1; i < sort.Length; i++)
+            {
+                if (sort[i] < min)
+                {
+                    min = sort[i];
+                }
+                else
+                {
+                    max = sort[i];
+                }
+            }
+            return min;
+        }
+
+        public int[] getValueUsingArrays(int[] sort)
+        {
+            int min = sort[0];
+            int max = sort[0];
+            for (int i = 1; i < sort.Length; i++)
+            {
+                if (sort[i] < min)
+                {
+                    min = sort[i];
+                }
+                else
+                {
+                    max = sort[i];
+                }
+            }
+
+            int[] arrays = new int[2] { min, max };
+            return arrays;
+        }
+
+        public Tuple<int,int> getValueUsingTuple(int[] sort)
+        {
+            int min = sort[0];
+            int max = sort[0];
+            for (int i = 1; i < sort.Length; i++)
+            {
+                if (sort[i] < min)
+                {
+                    min = sort[i];
+                }
+                else
+                {
+                    max = sort[i];
+                }
+            }
+            return Tuple.Create(min, max);
+        }
+
+        
+        public Result getValueUsingClass(int[] sort)
+        {
+            Result minmax = new Result();
+            minmax.min1 = sort[0];
+            minmax.max1 = sort[0];
+            for (int i = 1; i < sort.Length; i++)
+            {
+                if (sort[i] < minmax.min1)
+                {
+                    minmax.min1 = sort[i];
+                }
+                else
+                {
+                    minmax.max1 = sort[i];
+                }
+            }
+            return minmax;
+        }
+
+    }
+    public class Result
+    {
+        public int min1;
+        public int max1;
+    }
+}
---
+++ ArrayAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[7] { 11,32,13,53,3,15,69};
+            ArrayMinMax ar = new ArrayMinMax();
+
+            int max = 0;
+
+            int min = ar.getValueUsingRef(myArray, ref max);
+
+            Console.Write("The array is:  ");
+            foreach(int i in myArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\nMax: " + max + "      Min: " + min);
+
+            Console.WriteLine("\n--------------------");
+            min = ar.getValueUsingOut(myArray, out max);
+            Console.WriteLine("\nMax: " + max + "      Min: " + min);
+
+            Console.WriteLine("\n--------------------");
+            var res = ar.getValueUsingArrays(myArray);
+            Console.WriteLine("\nMax: " + res[1] + "      Min: " + res[0]);
+
+            Console.WriteLine("\n--------------------");
+            var res1 = ar.getValueUsingTuple(myArray);
+            Console.WriteLine("\nMax:" + res1.Item2 + "      Min: " + res1.Item1);
+
+            Console.WriteLine("\n--------------------");
+            var res2 = ar.getValueUsingClass(myArray);
+            Console.WriteLine("\nMax:" + res2.max1 + "      Min: " + res2.min1);
+
+            Console.ReadLine();
+        }
+    }
+
+    
+
+}
